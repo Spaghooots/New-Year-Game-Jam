4ac0cfcb809397cf47a28e26bc6b4dffc2910cde
Spaghooots <86173812+Spaghooots@users.noreply.github.com>
2024-01-07 19:26:03 -0800

    Add files via upload

---
+++ build/build.bat
@@ -0,0 +1,10 @@
+rem Change Name of Output file here
+set PROJECT_NAME=game
+
+set mypath=%~dp0
+cd "%mypath%"
+
+
+call emsdk activate latest
+call emcc -o %PROJECT_NAME%.html ../src/main.cpp -Os -Wall ../lib/libraylib.a -I ../include -L ../lib/libraylib.a -s USE_GLFW=3 -s ASYNCIFY -s GL_ENABLE_GET_PROC_ADDRESS --shell-file shell.html -DPLATFORM_WEB
+call emrun %PROJECT_NAME%.html
