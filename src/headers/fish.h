71ad876fa2a81f60da411967c73bae55a76eb6cc
Spaghooots <86173812+Spaghooots@users.noreply.github.com>
2024-01-11 01:18:45 -0800

    Finished base game (now just polish + extra features)

--- src/headers/fish.h
+++ src/headers/fish.h
@@ -1,13 +1,25 @@
+/* 
+All my fishies :) (Do not delete this comment!)
+    Fish headFish = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
+    Fish Fishron = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
+    Fish LeFishe = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
+    Fish Terry = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
+*/
+
+
+
 #include <raylib.h>
 #include <raymath.h>
 
 class Fish {
 
 private:
+
+    // Fish Variables
     Texture2D texture;
 
     float speed = 5;
-    float turnDampen = 10;
+    float turnDampen = 20;
     float rotation = 0;
 
     Vector2 direction;
@@ -19,41 +31,51 @@ private:
 
 public:
 
-    Fish(Texture2D texture, Vector2 position);
-    Vector2 getPosition() { return Vector2Subtract(position,Vector2Scale(Vector2Normalize(direction), texture.width/2 + 10)); }
+    // Fish Functions
+    Fish(Texture2D texture, Vector2 position, float turnDampen);
+    Vector2 getPosition() { return position; }
+    Vector2 getPositionFollow() { return Vector2Subtract(position,Vector2Scale(Vector2Normalize(direction), texture.width/2 + 10)); }
+    Texture2D getTexture() { return texture; }
     void Update(Vector2 desiredPosition);
     void Draw();
 };
 
-Fish::Fish(Texture2D texture, Vector2 position) {
+Fish::Fish(Texture2D texture, Vector2 position, float turnDampen) {
 
+    // Start facing to the right
     Fish::direction.x = 1;
     Fish::direction.y = 0;
 
+    // Find origin
     Fish::origin.x = texture.width/2;
     Fish::origin.y = texture.height/2;
 
-
+    // Set Constructor variables
+    Fish::turnDampen = turnDampen;
     Fish::texture = texture;
     Fish::position = position;
 
+    // Create slice of spritesheet (whole thing in this case)
     Fish::rectSlice = Rectangle{0, 0, (float)texture.width, (float)texture.height};
 }
 
 
 void Fish::Update(Vector2 desiredPosition) {
 
+    // Rotate fish to desired position
     float angle = Vector2Angle(Fish::direction, Vector2Negate(Vector2Subtract(Fish::position, desiredPosition)));
-
     Fish::direction = Vector2Rotate(Fish::direction, angle/turnDampen);
+
+    // Get angle for drawing
     Fish::rotation = Vector2Angle(Fish::direction, Vector2{1,0});
 
-
+    // Move forward
     Fish::position = Vector2Add(Fish::position, Vector2Scale(Vector2Normalize(Fish::direction), speed));
 
-    
+    // Update Draw rectangle
     Fish::rectDraw = Rectangle{position.x, position.y, (float)texture.width * 2, (float)texture.height};
 
+    // Flip fish
     if (Fish::direction.x < 0) {
         Fish::rectSlice.height = -Fish::texture.height;
     } else {
--- src/main.cpp
+++ src/main.cpp
@@ -12,50 +12,85 @@ int main(void) {
 
     InitWindow(screenWidth, screenHeight, "Template");
 
+    // Load Textures
     Texture2D chaserFishTexture = LoadTexture("assets/graphics/chaserFish.png");
     Texture2D cursorTexture = LoadTexture("assets/graphics/fish_food_cursor.png");
-
     
-    /* All my fishies (Do not delete this comment!)
-    Fish headFish = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
-    Fish Fishron = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
-    Fish LeFishe = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
-    Fish Terry = Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2});
-    */
-    
-    
+    // World Vectors
     std::vector<Food> foodItems;
     std::vector<Fish> fishies;
 
-    int fishAmount = 2;
+    // Fish Variables
+    int fishAmount = 1;
+    fishies.push_back(Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2}, 20));
 
-    fishies.push_back(Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2}));
-    fishies.push_back(Fish(chaserFishTexture, Vector2{0, 0}));
-
+    // Food Variables
     float foodTimer = 0;
     float foodSpawnDelaySeconds = 3.0f;
     int waterLevelY = 50;
 
+    // Player Variables
+    bool isAlive = true;
+    float playerHitRad = 10;
+
+
+    // Game Loop
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
 
-        if (IsKeyPressed(KEY_SPACE)) {
-            fishies.push_back(Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2}));
-            fishAmount ++;
+        // Check Collisions Fish
+        for (Fish fishie : fishies) {
+            if (CheckCollisionCircles(GetMousePosition(), playerHitRad, fishie.getPosition(), fishie.getTexture().height/2)) {
+                isAlive = false;
+            }
+        }
+
+        // Check Collision Food
+        for (int i = 0; i < foodItems.size(); i++) {
+            if (CheckCollisionCircles(GetMousePosition(), playerHitRad, Vector2{foodItems[i].getX(), foodItems[i].getY()}, foodItems[i].getRadius())) {
+                
+                // Spawn Fish
+                if(GetMouseX() < screenWidth/2) {
+                    fishies.push_back(Fish(chaserFishTexture, Vector2{screenWidth+20, (float)GetRandomValue(0, screenHeight)}, 10));
+                } else {
+                    fishies.push_back(Fish(chaserFishTexture, Vector2{-20, (float)GetRandomValue(0, screenHeight)}, 10));
+                }
+                fishAmount ++;
+
+                // Delete food
+                foodItems.erase(foodItems.begin() + i);
+            }
         }
         
-        fishies[0].Update(GetMousePosition());
-        for (int i = 1; i < fishAmount; i++) {
-            fishies[i].Update(fishies[i-1].getPosition());
+        // Check if alive
+        if (isAlive) {
+
+            // Fish Movement
+            fishies[0].Update(GetMousePosition());
+            for (int i = 1; i < fishAmount; i++) {
+                fishies[i].Update(fishies[i-1].getPositionFollow());
+            }
+
+            // Spawn Food
+            foodTimer += GetFrameTime();
+            if (foodTimer >= foodSpawnDelaySeconds)
+            {
+                foodItems.push_back(Food());
+                foodTimer = 0;
+            }
+        }
+        // Let player reset if dead
+        else if (IsKeyPressed(KEY_SPACE)) {
+            fishies = std::vector<Fish>{};
+            foodItems = std::vector<Food>{};
+            fishies.push_back(Fish(chaserFishTexture, Vector2{screenWidth/2, screenHeight/2}, 20));
+            fishAmount = 1;
+
+            isAlive = true;
         }
 
-        foodTimer += GetFrameTime();
-        if (foodTimer >= foodSpawnDelaySeconds)
-        {
-            foodItems.push_back(Food());
-            foodTimer = 0;
-        }
-
+        
+        // Delete food that falls
         for (int i = 0; i < foodItems.size(); i++)
         {
             foodItems[i].Update(waterLevelY);
@@ -70,19 +105,30 @@ int main(void) {
             ClearBackground(SKYBLUE);
             // Water
             DrawRectangle(0, waterLevelY, GetScreenWidth(), GetScreenHeight() - waterLevelY, BLUE);
+            
+            
+            if (isAlive) {
+                // Draw cursor if alive
+                DrawTexture(cursorTexture, GetMouseX(), GetMouseY(), WHITE);
+                
+            }
+            else {
+                // Draw death message when dead
+                DrawText("press space to reset", 100, 100, 20, WHITE);
+            }
 
-            DrawText("Press space to spawn a fish", 100, 100, 20, WHITE);
-
+            // Draw Fish
             for (Fish fishie : fishies) {
                 fishie.Draw();
             }
 
+            // Draw Food
             for (Food foodItem : foodItems)
             {
                 foodItem.Draw();
             }
 
-            DrawTexture(cursorTexture, GetMouseX(), GetMouseY(), WHITE);
+            
         EndDrawing();
 
     }
